9ee157219d2e7ed1d5d8ed5f2791fc663a96d0f8
unaG <veraguo11@163.com>
2024-01-26 10:15:24 +0800

    人老了老是写bug

--- inference.py
+++ inference.py
@@ -49,7 +49,7 @@ class CI_Inference:
             self._load_model(model_name)
             # image.convert('RGB')
             # pil_image = Image.fromarray(image.squeeze(0).numpy())  # TODO
-            pil_image = transforms.ToPILImage(image.squeeze(0).permute(1,2,0).contiguous())
+            pil_image = transforms.ToPILImage()(image.squeeze(0).permute(1,2,0).contiguous())
             prompt = self._interrogate(pil_image, mode)
         except Exception as e:
             prompt = ""
